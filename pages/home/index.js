d90131a96d714abbe2343176290702cc9649f02d
samuel leao <samuelleaoui@gmail.com>
2022-10-20 15:36:37 -0300

    saveColorsStyles and saveFontSizesStyles created

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,7 @@
 const inputsColor = [...document.querySelectorAll("input[type=color]")];
 const inputFontScale = document.querySelector("#fontScale");
 const buttonCopy = document.querySelector(".button-copy-colors");
+const buttonSaveStyles = document.querySelector(".button-save-colors");
 
 const transformVariablesColors = (input) => {
   const variablesColors = `
@@ -18,17 +19,59 @@ const transformVariablesFontsSize = (value, index) => {
   return variablesFontSizes;
 };
 
+let objectStyles = [];
+
+const saveColorsStyles = (input) => {
+  objectStyles.push(input.value);
+  window.localStorage.setItem("stylesColors", JSON.stringify(objectStyles));
+};
+
+const saveFontSizesStyles = (value) => {
+  window.localStorage.setItem("stylesFontSizes", JSON.stringify(value));
+};
+
+if (window.localStorage.getItem("stylesColors") !== null) {
+  let stylesLocalStorage = JSON.parse(
+    window.localStorage.getItem("stylesColors")
+  );
+
+  stylesLocalStorage.forEach((style, index) => {
+    inputsColor[index].value = style;
+  });
+
+  inputFontScale.value = JSON.parse(
+    window.localStorage.getItem("stylesFontSizes")
+  );
+}
+
+const changeTextButton = (element, textMoment, textFixed, delay) => {
+  element.textContent = textMoment;
+  element.style.pointerEvents = "none";
+
+  setTimeout(() => {
+    element.textContent = textFixed;
+    element.style.pointerEvents = "auto";
+  }, delay);
+};
+
+buttonSaveStyles.addEventListener("click", (event) => {
+  event.preventDefault();
+  inputsColor.map((input) => {
+    saveColorsStyles(input);
+  });
+  saveFontSizesStyles(inputFontScale.value);
+  changeTextButton(buttonSaveStyles, "Saved!", "Save Styles", 3000);
+
+  objectStyles = [];
+});
+
 buttonCopy.addEventListener("click", (event) => {
   let fontsSize = [];
   let variables = [];
 
   event.preventDefault();
 
-  buttonCopy.textContent = "Copied!";
-
-  setTimeout(() => {
-    buttonCopy.textContent = "Copy Styles";
-  }, 3000);
+  changeTextButton(buttonCopy, "Copied!", "Copy Styles", 3000);
 
   inputsColor.map((input) => {
     variables.push(transformVariablesColors(input));
