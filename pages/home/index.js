bc760e0112470f52fadfb0ca90adc19adceba0c2
samuel leao <samuelleaoui@gmail.com>
2022-10-20 16:00:14 -0300

    insert colors default

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,7 @@ const inputsColor = [...document.querySelectorAll("input[type=color]")];
 const inputFontScale = document.querySelector("#fontScale");
 const buttonCopy = document.querySelector(".button-copy-colors");
 const buttonSaveStyles = document.querySelector(".button-save-colors");
+const colors = ["#5f3dc4","#7048e8","#7950f2","#845ef7","#212529","#495057","#ced4da","#f1f3f5","#c92a2a","#087f5b","#f08c00","#c6c7cd"]
 
 const transformVariablesColors = (input) => {
   const variablesColors = `
@@ -42,6 +43,10 @@ if (window.localStorage.getItem("stylesColors") !== null) {
   inputFontScale.value = JSON.parse(
     window.localStorage.getItem("stylesFontSizes")
   );
+}else{
+  colors.forEach((style, index) => {
+    inputsColor[index].value = style;
+  });
 }
 
 const changeTextButton = (element, textMoment, textFixed, delay) => {
